33928b2bdb708936b96bd33da5c1ec4c2111bc80
Sarra <sarrasoussia2001@gmail.com>
2024-03-17 22:27:22 +0100

    sarra commit 1

--- projet/src/app/app-routing.module.ts
+++ projet/src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'home-page',
+    loadChildren: () => import('./pages/home-page/home-page.module').then( m => m.HomePagePageModule)
+  },
+
 ];
 
 @NgModule({
