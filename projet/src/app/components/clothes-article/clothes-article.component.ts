0e6cb0ed32ba09bab6af43b387d6bf554244584a
Sarra <sarrasoussia2001@gmail.com>
2024-03-24 04:31:35 +0100

    sarra created the add to cart and fav

--- projet/src/app/components/clothes-article/clothes-article.component.ts
+++ projet/src/app/components/clothes-article/clothes-article.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
-
+import { CartService } from 'src/app/services/cart.service';
+import { FavoriteService } from 'src/app/services/favorite.service';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-clothes-article',
   templateUrl: './clothes-article.component.html',
@@ -10,9 +13,29 @@ export class ClothesArticleComponent {
 
   isFavorite: boolean = false;
 
-  toggleFavorite() {
-    this.isFavorite = !this.isFavorite;
+  constructor(private toastController: ToastController, private cartService: CartService, private favService : FavoriteService, private router: Router) {}
+
+  toggleFavorite(article: any): void {
+    const index = this.favService.getFavItems().indexOf(article);
+    if (index !== -1) {
+      this.favService.removeFromFav(article);
+      console.log('Removed from favorites:', article.name);
+    } else {
+      this.favService.addToFav(article);
+      console.log('Added to favorites:', article.name);
+    }
+    this.isFavorite = !this.isFavorite; 
+    }
+
+  async addCart(item: any) {
+    this.cartService.addToCart(item);
+
+    const toast = await this.toastController.create({
+      message: 'Successfully added to cart',
+      duration: 500,
+      position: 'bottom' 
+    });
+    toast.present();
   }
-  
-  constructor() {}
+
 }
