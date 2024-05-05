63741e8583dd61fc8a38d1c4fca0582699d084c9
Ala <bhiriala577@gmail.com>
2024-05-05 19:37:25 +0100

    temchi al android

--- front/capacitor.config.ts
+++ front/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'projet',
-  webDir: 'src',
+  webDir: 'www',
   server: {
     androidScheme: 'https'
   }
